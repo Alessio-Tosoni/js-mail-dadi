aad986c3939c289d30f35aca51399c9e460e35e3
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-11-01 17:52:05 +0100

    termina l'esercizio

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
-const lista = [ "francesco@gmail.com", "andrea@gmail.com",  " bonanno@gmail.com", "lorenzo@gmail.com", "pasquale@gmail.com" ];
-let emailUtente = prompt("inserisci email")
+const lista = [ "francesco@gmail.com", "andrea@gmail.com",  " bonanno@gmail.com", "lorenzo@gmail.com", "pasquale@gmail.com", "a@gmail.com"];
+btn.addEventListener("click", function(){   
+    let emailUtente = document.getElementById("emailUtente").value;
+    if (lista.some(e=> e == emailUtente)){
+        document.getElementById("verificatore").innerHTML = "Sei autorizzato"
+        let dadoUtente = Math.floor(Math.random()*6)+1
+        document.getElementById("tu").innerHTML ="il tuo risultato  è : " + dadoUtente 
+        let dadoBrowser = Math.floor(Math.random()*6)+1
+        document.getElementById("computer").innerHTML = "il risultato del computer è : " + dadoBrowser
+        
+            if(dadoUtente > dadoBrowser){
+                document.getElementById("risultato").innerHTML = "Hai vinto"
+            } else{
+                document.getElementById("risultato").innerHTML = "Hai perso"
+            }
+    } else{
+        document.getElementById("verificatore").innerHTML = "Non sei autorizzato"
+    }
+})     
+        
 
-if (lista.some(e=> e == emailUtente)){
-    alert("sei autorizzato")
-    let dadoUtente = Math.floor(Math.random()*6)+1
-    console.log(dadoUtente)
-    let dadoBrowser = Math.floor(Math.random()*6)+1
-    console.log(dadoBrowser)
-        if(dadoUtente > dadoBrowser){
-            alert("hai vinto")
-        } else{
-            alert("hai perso")
-        }
-} else{
-    alert("non sei autorizzato")
-}
+        
